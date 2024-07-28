5c234c7a05672070f19bf9bf04bd5a4f17a87dee
Govindu-Thejana <govinduthejana@gmail.com>
2024-07-28 23:08:33 +0530

    Final commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,14 @@
 "use client";
-
-import React from "react";
-import { podcastData } from "@/constants";
 import PodcastCard from "@/components/PodcastCard";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import LoaderSpinner from "@/components/LoaderSpinner";
 
 const Home = () => {
   const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
-  
+
+  if (!trendingPodcasts) return <LoaderSpinner />;
+
   return (
     <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
       <section className="flex flex-col gap-5">
