bb4c955e7c71bceac95e48b6a84192bc75aa2bc6
Govindu-Thejana <govinduthejana@gmail.com>
2024-08-04 04:25:32 +0530

    Added podcastTable component to the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,14 +3,17 @@ import PodcastCard from "@/components/PodcastCard";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import LoaderSpinner from "@/components/LoaderSpinner";
+import { getLatestPodcasts } from "@/convex/podcasts";
+import PodcastTable from "@/components/PodcastTable";
 
 const Home = () => {
   const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
+  const latestPodcasts = useQuery(api.podcasts.getLatestPodcasts);
 
-  if (!trendingPodcasts) return <LoaderSpinner />;
+  if (!trendingPodcasts || !latestPodcasts) return <LoaderSpinner />;
 
   return (
-    <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
+    <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
 
@@ -20,13 +23,35 @@ const Home = () => {
 
         <div className="podcast_grid">
           {trendingPodcasts?.map(
-            ({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+            ({ _id, podcastTitle, podcastDescription, imageUrl, views }) => (
               <PodcastCard
                 key={_id}
                 imgUrl={imageUrl as string}
                 title={podcastTitle}
                 description={podcastDescription}
                 podcastId={_id}
+                views={views}
+              />
+            )
+          )}
+        </div>
+      </section>
+      <section className="flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">Latest Podcasts</h1>
+        <div className="flex flex-col">
+          {latestPodcasts?.map(
+            ({ _id, author, audioUrl, imageUrl, podcastTitle, audioDuration, views, _creationTime }, idx) => (
+              <PodcastTable
+                index={idx + 1}
+                key={_creationTime}
+                imgUrl={imageUrl as string}
+                title={podcastTitle}
+                podcastId={_id}
+                views={views}
+                duration={audioDuration}
+                audioUrl={audioUrl}
+                author={author}
+                creationTime={_creationTime}
               />
             )
           )}
