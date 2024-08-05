40e32a23d8f2fcc054ad7c62b1efffdb5db3dc0f
Govindu-Thejana <govinduthejana@gmail.com>
2024-08-05 22:36:11 +0530

    update podcastData in constats/index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,5 @@
+import { Id } from "@/convex/_generated/dataModel";
+
 export const sidebarLinks = [
     {
         imgURL: "/icons/home.svg",
@@ -50,57 +52,65 @@ export const voiceDetails = [
 
 export const podcastData = [
     {
-        id: 1,
+        id: "j57cfh3gdzfykx7bm73d7wrkbs6y5fap" as Id<"podcasts">,
         title: "The Joe Rogan Experience",
+        views: 46,
         description: "A long form, in-depth conversation",
         imgURL:
             "https://lovely-flamingo-139.convex.cloud/api/storage/3106b884-548d-4ba0-a179-785901f69806",
     },
     {
-        id: 2,
+        id: "j7cfh3gdzfykx7bm73d7wrkbs6y5fap" as Id<"podcasts">,
         title: "The Futur",
+        views: 15,
         description: "This is how the news should sound",
         imgURL:
             "https://lovely-flamingo-139.convex.cloud/api/storage/16fbf9bd-d800-42bc-ac95-d5a586447bf6",
     },
     {
-        id: 3,
+        id: "j5cfh3gdzfykx7bm73d7wrkbs6y5fap" as Id<"podcasts">,
         title: "Waveform",
+        views: 65,
         description: "Join Michelle Obama in conversation",
         imgURL:
             "https://lovely-flamingo-139.convex.cloud/api/storage/60f0c1d9-f2ac-4a96-9178-f01d78fa3733",
     },
     {
-        id: 4,
+        id: "j57cfhgdzfykx7bm73d7wrkbs6y5fap" as Id<"podcasts">,
         title: "The Tech Talks Daily Podcast",
+        views: 55,
         description: "This is how the news should sound",
         imgURL:
             "https://lovely-flamingo-139.convex.cloud/api/storage/5ba7ed1b-88b4-4c32-8d71-270f1c502445",
     },
     {
-        id: 5,
+        id: "j57cfh3gdzfykxbm73d7wrkbs6y5fap" as Id<"podcasts">,
         title: "GaryVee Audio Experience",
+        views: 60,
         description: "A long form, in-depth conversation",
         imgURL:
             "https://lovely-flamingo-139.convex.cloud/api/storage/ca7cb1a6-4919-4b2c-a73e-279a79ac6d23",
     },
     {
-        id: 6,
+        id: "j57cfh3gdzfykx7m73d7wrkbs6y5fap" as Id<"podcasts">,
         title: "Syntax ",
+        views: 85,
         description: "Join Michelle Obama in conversation",
         imgURL:
             "https://lovely-flamingo-139.convex.cloud/api/storage/b8ea40c7-aafb-401a-9129-73c515a73ab5",
     },
     {
-        id: 7,
+        id: "j57cfh3gzfykx7bm73d7wrkbs6y5fap" as Id<"podcasts">,
         title: "IMPAULSIVE",
+        views: 34,
         description: "A long form, in-depth conversation",
         imgURL:
             "https://lovely-flamingo-139.convex.cloud/api/storage/8a55d662-fe3f-4bcf-b78b-3b2f3d3def5c",
     },
     {
-        id: 8,
+        id: "j57ch3gdzfykx7bm73d7wrkbs6y5fap" as Id<"podcasts">,
         title: "Ted Tech",
+        views: 54,
         description: "This is how the news should sound",
         imgURL:
             "https://lovely-flamingo-139.convex.cloud/api/storage/221ee4bd-435f-42c3-8e98-4a001e0d806e",
