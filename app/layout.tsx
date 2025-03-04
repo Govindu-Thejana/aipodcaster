c536d4fd6c079b2a5d17793fe4df6b2f359bd2b5
Govindu <govinduthejana@gmail.com>
2025-03-04 15:38:44 +0530

    Add Google site verification meta tag to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
   return (
     <ConvexClerkProvider>
       <html lang="en">
+        <head>
+          <meta name="google-site-verification" content="d3gwcv8TghTh0PbnbbCj8-N7ok7zp2b1Zxh_n7JgNCo" />
+        </head>
         <AudioProvider>
           <body className={`${manrope.className}`}>{children}</body>
         </AudioProvider>
